d419048a8f8ccac81ca3dd03ba7ecce419b5ce31
Tyler Applebaum <applebaumt@ochin.org>
2017-09-13 08:44:05 -0700

    IIITTTTSSSS ALIIIIIIIVE
    Working example. Crap. I should not have committed my API key.

--- ThousandEyes-PSC.ps1
+++ ThousandEyes-PSC.ps1
@@ -1,58 +1,61 @@
 #http://developer.thousandeyes.com/v6/test_data/
+#$ISO8601_7DaysAgo = Get-Date (Get-Date).AddDays(-7) -format s
 
-Function Get-TETestInfo {
+Function script:Get-TETestInfo {
 [CmdletBinding()]
     param(
         [Parameter(ValueFromPipeline, HelpMessage="Specify the name of the test")]
-        [string]$TestName = "Logan's Minecraft Server"
+        [string]$TestName
 	)
-$apiuser = "tylerapplebaum@gmail.com"
-$apipassword = "1xviw1h3qt40531eh6q3ddvkt5uqa1jy"
-$authorization = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($apiuser + ":" + $apipassword))
-$headers = @{
+$ApiUser = "tylerapplebaum@gmail.com"
+$ApiPassword = "1xviw1h3qt40531eh6q3ddvkt5uqa1jy"
+$Authorization = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($ApiUser + ":" + $ApiPassword))
+$Headers = @{
 	"accept"= "application/json"; 
 	"content-type"= "application/json"; 
-	"authorization"= "Basic $authorization"
+	"authorization"= "Basic $Authorization"
 }
 
 $TestsResponse = Invoke-WebRequest https://api.thousandeyes.com/tests.json -Headers $headers
 $ParsedContent = ConvertFrom-Json $TestsResponse.Content
 $TestID = $ParsedContent.test | Where-Object testName -eq $TestName | Select -ExpandProperty testId
 $TestInterval = $ParsedContent.test | Where-Object testName -eq $TestName | Select -ExpandProperty interval
-Write-Output $TestID
-Write-Output $TestInterval
-}
+Write-Verbose "Selected test ID $TestID"
+} #End Get-TETestInfo
 
 
-##### Run code below to gather data; append to CSV
-
-$apiuser = "tylerapplebaum@gmail.com"
-$apipassword = "1xviw1h3qt40531eh6q3ddvkt5uqa1jy"
-$authorization = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($apiuser + ":" + $apipassword))
-
-$HeadersTest = @{
-	"accept"= "application/json"; 
-	"content-type"= "application/json"; 
-	"authorization"= "Basic $authorization"
-}
+Function script:Get-TETestNetMetrics {
 
 $QueryString = @{
-	"window" = "7d"
+	"window" = "7d" #Adjust this based on the amount of data wanted
 }
-$ISO8601_7DaysAgo = Get-Date (Get-Date).AddDays(-7) -format s
 
-$response3 = Invoke-WebRequest "https://api.thousandeyes.com/v6/net/metrics/$TestID" -Headers $HeadersTest -Body $QueryString
+$MetricsResponse = Invoke-WebRequest "https://api.thousandeyes.com/v6/net/metrics/$TestID" -Headers $Headers -Body $QueryString
 
+#PowerShell's native ConvertFrom-Json cuts off the data, so a .NET assembly was used to do it right!
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
-$jsonserial= New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
-$jsonserial.MaxJsonLength  = 67108864
-$Obj = $jsonserial.DeserializeObject($Response3)
+$JsonSerial= New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
+$JsonSerial.MaxJsonLength  = 67108864
+$RawPSObject = $JsonSerial.DeserializeObject($MetricsResponse)
 
-$Obj.net.metrics.date
-$Obj.net.metrics.loss
-$Obj.net.metrics.jitter
-$Obj.net.metrics.minLatency
-$Obj.net.metrics.avgLatency
-$Obj.net.metrics.maxLatency
+$i = 0 #Count for each item in the array of metric data
+$OutputList = New-Object System.Collections.Generic.List[System.Object]
+ For ($y = 1; $y -le $RawPSObject.net.metrics.count; $y++){
+	$FilteredPSObject = [PSCustomObject]@{
+		"Date (UTC)" = $RawPSObject.net.metrics.date[$i]
+		"Loss" = $RawPSObject.net.metrics.loss[$i]
+		"Jitter" = $RawPSObject.net.metrics.jitter[$i]
+		"Avg RTT" = $RawPSObject.net.metrics.avgLatency[$i]
+		"Min RTT" = $RawPSObject.net.metrics.minLatency[$i]
+		"Max RTT" = $RawPSObject.net.metrics.maxLatency[$i]
+	}
+$OutputList.Add($FilteredPSObject)
+$i++
+}
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$OutputList | Export-CSV $DesktopPath\$TestName.csv -NoTypeInformation
+  
+} #End Get-TETestNetMetrics
 
-
+. Get-TETestInfo -TestName "Logan's Minecraft Server"
+. Get-TETestNetMetrics
